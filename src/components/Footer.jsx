ea0d36c2a36423eb849e05c7e8cc012c2a20ff2a
hanaf <hanaf.othmane@gmail.com>
2021-01-19 07:13:18 -0500

    copyright symbol

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const year = new Date().getFullYear();
 
 function Footer(){
-    return (<footer><p>Copyright Othmane Hanaf {year}</p></footer>);
+    return (<footer><p>Copyright © Othmane Hanaf {year}</p></footer>);
 };
 
 export default Footer;
