ebbd8aa6df708eaebec7d3f81c21384514709f52
hanaf <hanaf.othmane@gmail.com>
2021-01-22 07:55:26 -0500

    render note component w map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note"
+import Note from "./Note";
+import Notes from "../notes.js";
 
+console.log(Notes);
 
 function App(){
-    return (
-        <div>
-            <Header />
-            <Note />
-            <Footer />
-        </div>
-    );
-};
+    return(
+    <div>
+    <Header />
+    {Notes.map(noteInfo => (<Note key={noteInfo.key} title={noteInfo.title} content={noteInfo.content}/>))}
+    <Footer />
+    </div>);
+}
 
 export default App;
